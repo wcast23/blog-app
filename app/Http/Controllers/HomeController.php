d8639e14f89cad7a20dde57fbcd69c9112aa0fb3
Wilfredo Castaneda <wilfredo.castaneda.guerrero@gmail.com>
2025-05-21 13:20:20 -0600

    refactor: controllers and views
    Add functionality to retrieve categories in the main layout
    Refactor controller to use eloquent ORM

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = ['Category 1', 'Category 2'];
+        //$allCategories is the object that will contain
+        //the data retrieved from the DB
+        $allCategories = Category::all();
 
+        //We retrieve the data into the view named "home"
         return view('home', ['categories' => $allCategories]);
     }
 }
